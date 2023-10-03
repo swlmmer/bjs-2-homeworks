3083deafea321f5df5a642a8514a774b68b7c609
swlmmer <marsgalaktikov@yandex.ru>
2023-10-03 18:50:20 +0300

    Реализовано 1-е задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+function solveEquation(name, ...roots) {
+  let a = 10;
+  let b = 10;
+  let c = 10;
+  let d = Math.pow(b, 2) - 4*a*c;
+  console.log(`Ответ: ${name}`);
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (d < 0) {
+    let root = 0;
+    console.log(`${roots}`);
+    return root;
+  } else if (d === 0) {
+    let root1 = -b/2*a;
+    console.log(`${roots}`);
+  } else if (d > 0) {
+    let root2 = (-b + Math.sqrt(d) )/(2*a);
+    let root3 = (-b - Math.sqrt(d) )/(2*a);
+    console.log(`${roots}`);
+  }
 }
+  
+  solveEquation('Решение уравнения: ', root, root1, root2, root3);
+  
